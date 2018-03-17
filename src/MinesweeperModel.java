c3a4b96f2179e490e48647591ccb510d1d05925c
ygold <ygold@GOLDINLAPTOP.T-mobile.com>
2018-03-17 16:21:06 -0700

    build up how many mines are around each square

--- src/MinesweeperModel.java
+++ src/MinesweeperModel.java
@@ -10,7 +10,7 @@ public class MinesweeperModel {
 		if(rows <= 0 || columns <= 0 || minesToPlace > rows*columns) {
 			throw new IllegalArgumentException();
 		}
-		mines = minesToPlace;
+		
 		gameLayout = new int[rows][columns];
 		playersViewLayout = new char[rows][columns];
 		for(int i = 0; i < rows; i++) {
@@ -18,7 +18,17 @@ public class MinesweeperModel {
 				playersViewLayout[i][j] = '-';
 			}
 		}
+		
+		mines = minesToPlace;
 		placeMines();
+		
+		for(int r = 0; r < gameLayout.length; r++) {
+			for(int c = 0; c < gameLayout[0].length; c++) {
+				if(gameLayout[r][c] != MINE_IDENTIFIER) {
+					gameLayout[r][c] = identifySurroundingMines(r, c);
+				}
+			}
+		}
 	}
 	
 	private void placeMines() {
@@ -34,4 +44,23 @@ public class MinesweeperModel {
 			}
 		}
 	}
+	
+	private int identifySurroundingMines(int row, int col) {
+		assert(gameLayout[row][col] != MINE_IDENTIFIER);
+		int sum = 0;
+		for(int i = row - 1; i <= row + 1; i++) {
+			for(int j = col - 1; j <= col + 1; j++) {
+				if(inBounds(i, j) && gameLayout[i][j] == MINE_IDENTIFIER) {
+					sum++;
+				}
+			}
+		}
+		return sum;
+	}
+	
+	private boolean inBounds(int row, int col) {
+		return row >= 0 && row < gameLayout.length && col >= 0 && col < gameLayout[0].length;
+	}
+	
+	
 }
